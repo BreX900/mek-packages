54b102b4444170d00cbf6a3fb5babd176d2f73d2
BreX900 <brexmaster@gmail.com>
2023-07-17 16:34:38 +0200

    refactor: clean project and fixs gitignores

--- mek_stripe_terminal/android/src/main/kotlin/mek_stripe_terminal/Extensions.kt
+++
@@ -1,5 +0,0 @@
-package mek_stripe_terminal
-
-fun <K, V> Map<K, V>.toHashMap(): HashMap<K, V> {
-    return hashMapOf(*map { (k, v) -> k to v }.toTypedArray())
-}
--- mek_stripe_terminal/example/android/app/src/main/kotlin/com/example/stripe/terminal/mek_stripe_terminal_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.stripe.terminal.mek_stripe_terminal_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}

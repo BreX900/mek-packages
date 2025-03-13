a3edcd6976492b7635a26f90a1f7ce66014f3d21
Hannes Rüger <hannes@rueger.events>
2025-03-13 17:33:06 +0100

    feat: support loading currency from env

--- stripe_terminal/example/lib/models/k.dart
+++ stripe_terminal/example/lib/models/k.dart
@@ -1,3 +1,5 @@
+const envCurrency = String.fromEnvironment('CURRENCY');
+
 abstract final class K {
-  static const String currency = 'gbp';
+  static const String currency = envCurrency == '' ? 'gbp' : envCurrency;
 }
